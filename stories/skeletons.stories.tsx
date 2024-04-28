afa0727e8ef420d0e3968a695a094821003e4ee3
MiloMT <myles@mylestaylor.dev>
2024-04-28 15:35:39 +1000

    Added skeletons to storybook

--- stories/skeletons.stories.tsx
+++ stories/skeletons.stories.tsx
@@ -1,10 +1,51 @@
-import CardSkeleton from "@/app/ui/skeletons";
+import DashboardSkeleton, 
+{ CardSkeleton, 
+  CardsSkeleton, 
+  RevenueChartSkeleton, 
+  InvoiceSkeleton,
+  LatestInvoicesSkeleton,
+  TableRowSkeleton,
+  InvoicesMobileSkeleton,
+  InvoicesTableSkeleton
+} from "@/app/ui/skeletons";
 
 export default {
-  title: "Components/Skeletons/Card",
-  component: CardSkeleton,
+  title: "Components/Skeletons",
+  component: DashboardSkeleton,
 }
 
 export const CardSkele = () => (
   <CardSkeleton />
+)
+
+export const CardsSkele = () => (
+  <CardsSkeleton />
+)
+
+export const RevenueChartSkele = () => (
+  <RevenueChartSkeleton />
+)
+
+export const InvoiceSkele = () => (
+  <InvoiceSkeleton />
+)
+
+export const LatestInvoicesSkele = () => (
+  <LatestInvoicesSkeleton />
+)
+
+export const TableRowSkele = () => (
+  <TableRowSkeleton />
+)
+
+export const InvoicesMobileSkele = () => (
+  <InvoicesMobileSkeleton />
+)
+
+export const InvoicesTableSkele = () => (
+  <InvoicesTableSkeleton />
+)
+
+export const DashboardSkele = () => (
+  <DashboardSkeleton />
 )
